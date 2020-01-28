be7e47bd97d9e26901b10e3f8e11b680832dfa70
david.baena <david.baena@bq.com>
2020-01-28 08:29:07 +0100

    Initial commit

---
+++ app/src/main/java/com/davidbaena/arena/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.davidbaena.arena
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "WELCOME TO ARENA", Toast.LENGTH_LONG).show()
+    }
+}
